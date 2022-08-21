3e4504db660c265c67d1bdb397d4a3b0e8996a62
MikhailKushpil93 <misha-ua@i.ua>
2022-08-21 15:33:09 +0300

    interface

--- interface.py
+++ interface.py
@@ -1,57 +1,25 @@
+print('виберіть тип акаунта')
+print('админ - 1')
+print('пользуватель - 2')
+d = int(input())
 
-def interface():
-    command = input("""
-    Введіть одну з наступных команд:
-    n  - створити аккаунт
-    r  - змінити дані 
-    d  - видалити сторінку
-    """)
+if d==1:
+    admins = open('admin.txt', 'a', encoding='utf-8')
 
-    if command == "n":
-        print("Режим new!")
-        name = input("Ведіть ім'я: ")
-        password = input("Ведіть пароль: ")
-        name_password = f"{name} : {password}\n"
-        f = open("text.txt", "a")
-        f.write(name_password)
-        f.close()
-    elif command == "r":
-        print("Режим red!")
-        old_name = input("Ведіть ім'я: ")
-        old_password = input("Ведіть пароль: ")
-        old_name_password = f"{old_name} : {old_password}"
-    #     # new_name = input("Ведіть нове ім'я: ")
-    #     # new_password = input("Ведіть новий пароль: ")
-    #     # new_name_password = f"{new_name} : {new_password}\n"
+    login = input("login:")
+    passworld = input("password:")
+    discord = input("discord:")
 
-        f = open("text.txt", "r")
-        lines = f.readlines()
-        f.close()
-        print(lines)
+    admins.write(f"'login' : {login} , 'password' : {passworld}, 'discord' : {discord}, 'type_acc' : {d}\n")
+    print('Добро пожалу1вати на админ акаунт')
+    admins.close()
 
-        f = open("text.txt", "w")
-        for line in lines:
-            if line.replace("\n","") != old_name_password:
-                f.write(line)
-            else:
-                new_name = input("Ведіть нове ім'я: ")
-                new_password = input("Ведіть новий пароль: ")
-                new_name_password = f"{new_name} : {new_password}\n"
-                f.write(new_name_password)
-        f.close()
-    elif command == "d":
-        name = input("Ведіть ім'я на видалення: ")
-        password = input("Ведіть пароль на видалення: ")
-        name_password_del = f"{name} : {password}"
+    f = open("admin.txt", "r")
+    lines = f.readlines()
+    f.close()
+    # print(lines)
+    
 
-        f = open("text.txt", "r")
-        lines = f.readlines()
-        f.close()
+    for line in lines:
+        print(str(line))
 
-        f = open("text.txt", "w")
-        for line in lines:
-            if line.replace("\n","") != name_password_del:
-                f.write(line)
-            else:
-                print(f"Аккаунт видалено!!!")
-        f.close()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-file = open('login.txt', 'a')
+# file = open('login.txt', 'a')
 
-a = input("login:")
-b = input("passworld:")
-pin = f"{a} : {b} \n"
-file.write(pin)
+# a = input("login:")
+# b = input("passworld:")
+# pin = f"{a} : {b} \n"
+# file.write(pin)
 
 print('виберіть тип акаунта')
 print('админ - 1')
@@ -13,16 +13,23 @@ d = int(input())
 if d==1:
     admins = open('admin.txt', 'a', encoding='utf-8')
 
-    y = input("login:")
-    w = input("passworld:")
-    u = input("discord:")
-    mun =  f"{y} \n " #логін
-    mun1 = f"{w} \n " #пароль
-    mun2 = f"{u} \n " #дискорд
-    admins.write(mun)
-    admins.write(mun1)
-    admins.write(mun2)
+    login = input("login:")
+    passworld = input("password:")
+    discord = input("discord:")
+
+    admins.write(f"'login' :1 {login} , 'password' : {passworld}, 'discord' : {discord}, 'type_acc' : {d}\n")
     print('Добро пожалувати на админ акаунт')
 
-if d==2:
-    print('ви успішно зарегіструвалися як пользуватель ')
+    f = open("admin.txt", "r")
+    lines = f.readlines()
+    f.close()
+    print(lines)
+
+    f = open("admin.txt", "w")
+    for line in lines:
+        print(line)
+
+
+
+# if d==2:
+#     print('ви успішно зарегіструвалися як пользуватель ')
